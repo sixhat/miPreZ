5349ed7be4477eede338fed4faaef2a17989190a
David Sousa-Rodrigues <david@sixhat.net>
2022-03-03 16:01:50 +0000

    Conversion to JS nav (simpler)

---
+++ assets/app.js
@@ -0,0 +1,40 @@
+const slides = document.getElementsByClassName('slide')
+let nav
+function myMouseClicked() {
+    let url = window.location.href
+    const urls = url.split('#s')
+    if (window.event.clientX > 0.6 * document.documentElement.clientWidth) {
+        let t = (parseInt(urls[1]) + 1) % slides.length
+        if (isNaN(t)) {
+            t = 1
+        }
+        url = urls[0] + "#s" + t
+    }
+    if (window.event.clientX < 0.4 * document.documentElement.clientWidth) {
+        const t = parseInt(urls[1]) > 0 ? parseInt(urls[1]) - 1 : 0
+        url = urls[0] + "#s" + t
+    }
+    console.log(url);
+    window.location.href = url
+}
+
+if (slides.length > 0) {
+    nav = document.createElement("NAV")
+}
+
+for (let i = 0; i < slides.length; i++) {
+    slides[i].id = 's' + i
+    // const p = document.createElement("P")
+    // p.classList.add('next')
+    // const a = document.createElement("A")
+    // a.setAttribute('href', '#s' + (i + 1) % slides.length)
+    // a.innerHTML = 'next'
+    // p.appendChild(a)
+    // // slides[i].appendChild(p)
+    const aa = document.createElement("A")
+    aa.setAttribute('href', '#s' + i)
+    aa.innerHTML = i + 1
+    nav.appendChild(aa)
+}
+document.body.appendChild(nav)
+document.body.addEventListener('click', myMouseClicked)
